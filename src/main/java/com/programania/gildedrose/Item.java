f2bfe879afb4fb4909406168a623f9b9924af395
Guille <guille@ggalmazor.com>
2013-10-03 12:33:16 +0200

    Reformatted to two-spaces indentation

--- src/main/java/com/programania/gildedrose/Item.java
+++ src/main/java/com/programania/gildedrose/Item.java
@@ -1,37 +1,37 @@
 package com.programania.gildedrose;
 
 public class Item {
-    public String name;
-    public int sellIn;
-    public int quality;
+  public String name;
+  public int sellIn;
+  public int quality;
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+  public Item(String name, int sellIn, int quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public int getSellIn() {
-        return sellIn;
-    }
+  public int getSellIn() {
+    return sellIn;
+  }
 
-    public void setSellIn(int sellIn) {
-        this.sellIn = sellIn;
-    }
+  public void setSellIn(int sellIn) {
+    this.sellIn = sellIn;
+  }
 
-    public int getQuality() {
-        return quality;
-    }
+  public int getQuality() {
+    return quality;
+  }
 
-    public void setQuality(int quality) {
-        this.quality = quality;
-    }
+  public void setQuality(int quality) {
+    this.quality = quality;
+  }
 }
